f69bbeb867e37fe75b29b8b440df2e72f88b77b3
aldomv2 <aldomv2@hotmail.com>
2021-03-24 14:01:18 -0600

    Ajustes
    Cambios adicionales 24

--- src/main/java/com/mqclient/DemoApplication.java
+++ src/main/java/com/mqclient/DemoApplication.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 //Cambios para git
 //Cambios para el branch de cambios adicionales 
+//
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/mqclient/controller/MQController.java
+++ src/main/java/com/mqclient/controller/MQController.java
@@ -13,4 +13,5 @@ public class MQController {
 		  
 	  return   "OK SERVICIO DEMO";
 	}
+	
 }
